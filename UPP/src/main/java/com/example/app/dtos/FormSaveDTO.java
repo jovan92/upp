6a53e9c743ef600e2b3e2a08e23b4bfdf9e2e736
Novica <nole0223@gmail.com>
2020-12-30 04:39:09 +0100

    delete repo

--- UPP/src/main/java/com/example/app/dtos/FormSaveDTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.app.dtos;
-
-public class FormSaveDTO {
-
-}
--- UPP/src/main/java/com/example/app/services/ServiceUtils.java
+++
@@ -1,8 +0,0 @@
-package com.example.app.services;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class ServiceUtils {
-}
